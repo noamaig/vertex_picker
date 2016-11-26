bb5dd089c17c74d9255256ab8b68c33f214ccee6
noam <noam.aigerman@weizmann.ac.il>
2016-11-26 21:16:29 +0200

    Better selection of points using patch's callback

--- callbackClickA3DPoint.m
+++ callbackClickA3DPoint.m
@@ -1,4 +1,4 @@
-function callbackClickA3DPoint(src, eventData, pointCloud,T,valid,logger)
+function callbackClickA3DPoint(src, eventData, V,valid,logger)
 % CALLBACKCLICK3DPOINT mouse click callback function for CLICKA3DPOINT
 %
 %   The transformation between the viewing frame and the point cloud frame
@@ -13,51 +13,53 @@ function callbackClickA3DPoint(src, eventData, pointCloud,T,valid,logger)
 %   revised Jun 3, 2008
 %   revised May 19, 2009
 
-point = get(gca, 'CurrentPoint'); % mouse click position
-camPos = get(gca, 'CameraPosition'); % camera position
-camTgt = get(gca, 'CameraTarget'); % where the camera is pointing to
-
-camDir = camPos - camTgt; % camera direction
-camUpVect = get(gca, 'CameraUpVector'); % camera 'up' vector
-
-% build an orthonormal frame based on the viewing direction and the 
-% up vector (the "view frame")
-zAxis = camDir/norm(camDir);    
-upAxis = camUpVect/norm(camUpVect); 
-xAxis = cross(upAxis, zAxis);
-yAxis = cross(zAxis, xAxis);
-
-rot = [xAxis; yAxis; zAxis]; % view rotation 
-
-% the point cloud represented in the view frame
-rotatedPointCloud = rot * pointCloud(:,valid); 
-% figure(2);
-% clf
-% plot3(rotatedPointCloud(1,:), rotatedPointCloud(2,:), rotatedPointCloud(3,:), 'c.'); 
-% axis equal
-% pause
-% the clicked point represented in the view frame
-rotatedPointFront = rot * point' ;
+% point = get(gca, 'CurrentPoint'); % mouse click position
+% camPos = get(gca, 'CameraPosition'); % camera position
+% camTgt = get(gca, 'CameraTarget'); % where the camera is pointing to
+% 
+% camDir = camPos - camTgt; % camera direction
+% camUpVect = get(gca, 'CameraUpVector'); % camera 'up' vector
+% 
+% % build an orthonormal frame based on the viewing direction and the 
+% % up vector (the "view frame")
+% zAxis = camDir/norm(camDir);    
+% upAxis = camUpVect/norm(camUpVect); 
+% xAxis = cross(upAxis, zAxis);
+% yAxis = cross(zAxis, xAxis);
+% 
+% rot = [xAxis; yAxis; zAxis]; % view rotation 
+% 
+% % the point cloud represented in the view frame
+% rotatedPointCloud = rot * pointCloud(:,valid); 
+% % figure(2);
+% % clf
+% % plot3(rotatedPointCloud(1,:), rotatedPointCloud(2,:), rotatedPointCloud(3,:), 'c.'); 
+% % axis equal
+% % pause
+% % the clicked point represented in the view frame
+% rotatedPointFront = rot * point' ;
 
 % find the nearest neighbour to the clicked point 
 % pointCloudIndex = dsearchn(rotatedPointCloud(1:2,:)', ... 
 %     rotatedPointFront(1:2));
-dists = sum(bsxfun(@(rowA,rowB)(rowA-rowB), rotatedPointCloud(1:2,:)',rotatedPointFront(1:2,1)').^2,2);
-inds=find(dists<1e-4);
-if isempty(inds)
-    disp('no point close enough to click');
-    return;
-end
-candidates=rotatedPointCloud(:,inds);
-candidates(3,:)=candidates(3,:)-max(candidates(3,:));
-d=sum(candidates.^2);
-[~,highest]=min(d);
-
-pointCloudIndex=inds(highest);
-pointCloudIndex=valid(pointCloudIndex);
-% h = findobj(gca,'Tag','pt'); % try to find the old point
-selectedPoint = pointCloud(:, pointCloudIndex); 
-
+% dists = sum(bsxfun(@(rowA,rowB)(rowA-rowB), rotatedPointCloud(1:2,:)',rotatedPointFront(1:2,1)').^2,2);
+% inds=find(dists<1e-4);
+% if isempty(inds)
+%     disp('no point close enough to click');
+%     return;
+% end
+% candidates=rotatedPointCloud(:,inds);
+% candidates(3,:)=candidates(3,:)-max(candidates(3,:));
+% d=sum(candidates.^2);
+% [~,highest]=min(d);
+% 
+% pointCloudIndex=inds(highest);
+% pointCloudIndex=valid(pointCloudIndex);
+% % h = findobj(gca,'Tag','pt'); % try to find the old point
+% selectedPoint = pointCloud(:, pointCloudIndex); 
+p=eventData.IntersectionPoint;
+ind=knnsearch(V(valid,:),p);
+ind=valid(ind);
 % if isempty(h) % if it's the first click (i.e. no previous point to delete)
 %     
 %     % highlight the selected point
@@ -75,6 +77,6 @@ selectedPoint = pointCloud(:, pointCloudIndex);
 %     set(h,'Tag','pt');  % set its Tag property for later use
 % 
 % end
-logger.setCurPoint(selectedPoint',pointCloudIndex);
+logger.setCurPoint(V(ind,:),ind);
 logger.draw();
-fprintf('you clicked on point number %d\n', pointCloudIndex);
+fprintf('you clicked on point number %d\n', ind);
--- clickA3DPoint.m
+++ clickA3DPoint.m
@@ -9,17 +9,13 @@ disp('click ''n'' to progress to adding a *n*ew point while saving the prev');
 disp('click ''d'' to *d*elete the last inserted point');
 disp('click ''s'' to *s*ave and print the selected points to workspace');
 
-V=V';
-T=T';
-if size(V, 1)~=3
-    error('Input point cloud must be a 3*N matrix.');
-end
+
 
 % show the point cloud
 
 hold off
 % plot3(V(1,:), V(2,:), V(3,:), 'c.'); 
-h=patch('vertices',V','faces',T','facecolor',[0.9 0.9 0.9],'edgecolor','none');
+h=patch('vertices',V,'faces',T,'facecolor',[0.9 0.9 0.9],'edgecolor','none');
 light('Position',[1 0 0],'Style','local','color','cyan')
 light('Position',[0 -1  0],'Style','local','color','magenta')
 light('Position',[0 0 -1],'Style','local','color','yellow')
@@ -34,7 +30,7 @@ hold on; % so we can highlight clicked points without clearing the figure
 axis equal
 % set the callback, pass pointCloud to the callback function
 logger=pointLogger(name);
-tri=triangulation(T',V');
+tri=triangulation(T,V);
 b=tri.freeBoundary();
 if ~isempty(b)
     b=b(:,1);
@@ -42,10 +38,11 @@ if ~isempty(b)
 else
     b=1:length(V);
 end
-b=b(:,1);
-h=gcf;
-set(h, 'WindowButtonDownFcn', {@callbackClickA3DPoint, V,T,b,logger}); 
-set(h,'KeyPressFcn',{@keyHandler,logger });
+hf=gcf;
+set(h,'ButtonDownFcn',{@callbackClickA3DPoint,V,b,logger},...
+  'PickableParts','visible');
+% set(h, 'WindowButtonDownFcn', {@callbackClickA3DPoint, V,T,b,logger}); 
+set(hf,'KeyPressFcn',{@keyHandler,logger });
 end
 function keyHandler(h_obj,evt,logger)
 if strcmp(evt.Key,'n')
